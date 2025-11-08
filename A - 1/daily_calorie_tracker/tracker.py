444057fe3a9253f9f4415fd6805c3e9553e438f5
lucky-KRMU <2501730142@krmu.edu.in>
2025-11-08 09:21:19 +0000

    create necessary files for A-1

---
+++ A - 1/daily_calorie_tracker/tracker.py
@@ -0,0 +1,14 @@
+
+# Name: Lucky Pawar
+# Date: November 8th, 2025
+# Project title: Daily Calorie Tracker (CLI)
+
+welcome_str = '''
+#===============================#
+#             WELCOME           #
+#-------------------------------#
+#*******************************#
+#   Daily Calorie Tracker(CLI)  #
+#*******************************#
+'''
+print(welcome_str)
