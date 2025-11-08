ae328ca64ec758d2912d120f0b71d4a1c7b526e9
lucky-KRMU <2501730142@krmu.edu.in>
2025-11-08 09:27:07 +0000

    update print message

--- A - 1/daily_calorie_tracker/tracker.py
+++ A - 1/daily_calorie_tracker/tracker.py
@@ -3,12 +3,26 @@
 # Date: November 8th, 2025
 # Project title: Daily Calorie Tracker (CLI)
 
-welcome_str = '''
+# Creating a welcome string
+welcome_str = ''' 
 #===============================#
 #             WELCOME           #
 #-------------------------------#
 #*******************************#
 #   Daily Calorie Tracker(CLI)  #
 #*******************************#
+
+> Why use this tool?  :- )
+=> This is the tool that can be 
+used to calculate and keep a track
+of your daily calorie intake.
+
+**********************************
+#================================#
+# Thanks for using our tool! :-D #
+#================================#
 '''
-print(welcome_str)
+
+print(welcome_str)  # printing the welcome string
+
+
